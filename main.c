e209f219f66ab9d1b7996ddfad918ede681499e2
Vadim Ostafiychuk <vadym.ostafiychuk@riseserv.com>
2020-09-16 11:25:56 +0300

    Init

---
+++ main.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main(void)
+{
+	setlocale(LC_ALL, "ua");
+	int x, c;
+
+	printf("Введiть число x: ");
+	scanf("%d", &x);
+
+
+	if(x == 0) {
+		printf("Число нуль");
+	}
+	else if (x % 2 == 0 && x > 0)
+	{
+		printf("Число %d парне додатне число", x);
+	}
+	else if (x % 2 == 0 && x < 0) {
+		printf("Число %d парне від'ємне число", x);
+	}
+	else if (x % 2 != 0 && x < 0) {
+		printf("Число %d непарне від'ємне число", x);
+	}
+	else {
+		printf("Число %d непарне додатне число", x);
+	}
+
+	printf("\n\n");
+	
+	return;
+}
